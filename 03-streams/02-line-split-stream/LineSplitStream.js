fb479e11c8550792ba8154455a1590b4c4136ee0
Roman Vazhynskyi <rvazhynskyi@gmail.com>
2021-10-05 00:39:07 +0300

    Added a solution for the task 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -7,9 +7,33 @@ class LineSplitStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    const parts = chunk.toString().split(os.EOL);
+
+    if (this.lastPart) {
+      this.push(this.lastPart + parts.shift());
+      this.lastPart = '';
+    }
+
+    if (!this._endsWithEOL(chunk.toString())) {
+      this.lastPart = parts.pop();
+    }
+
+    parts.forEach((part) => {
+      this.push(part);
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    if (this.lastPart) {
+      this.push(this.lastPart);
+    }
+    callback();
+  }
+
+  _endsWithEOL(chunk) {
+    return chunk.length === (chunk.lastIndexOf(os.EOL) + 1);
   }
 }
 
